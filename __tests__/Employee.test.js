3460c98aa7f698d8394211e3be9f4de0c0a3f3c8
Jyothy Baby <jyothy.v.baby@gmail.com>
2021-08-27 14:41:11 -0400

    updating test files

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,10 +1,13 @@
+//https://www.freecodecamp.org/news/how-to-start-unit-testing-javascript/ --- For reference
+// importing Employee.js
 const Employee = require("../lib/Employee");
 
+// Writing the test for employee instance
 test("Can instantiate Employee instance", () => {
-  const e = new Employee();
+  const e = new Employee("jyothy", 1, "test@test.com");
   expect(typeof(e)).toBe("object");
 });
-
+// startrd testing for constructor arguments (name,id,email)
 test("Can set name via constructor arguments", () => {
   const testValue = "jyothy";
   const e = new Employee(testValue, 1, "test@test.com");
@@ -22,11 +25,11 @@ test("Can set email via constructor argument", () => {
   const e = new Employee("jyothy", 1, testValue);
   expect(e.email).toBe(testValue);
 });
-
+// test suite for  functions 
 test("Can get name via getName()", () => {
   const testValue = "jyothy";
   const e = new Employee(testValue);
-  expect(e.getName()).toBe(testValue);
+  expect(e.getName()).toBe(testValue, 1, "test@test.com");
 });
 
 test("Can get id via getId()", () => {
