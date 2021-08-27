3347cd1b89567b91d04fc561ec5a05587fb056c3
Jyothy Baby <jyothy.v.baby@gmail.com>
2021-08-27 11:20:50 -0400

    followed Insturctor's instructions to create Html template

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,10 +1,16 @@
-const Manager = require("../manager");
+const Manager = require("./lib/Manager");
+const Employee = require("./lib/Employee");
 
-    test("shouls be create an object with parameter if provided valid arguments", ()=>{
-        const manager = new Manager("Sarah,01,abc@abc.com,1234567890");
-        expect(manager.name).toEqual("sarah");
-        expect(manager.id).toEqual(01);
-        expect(manager.email).toEqual("abc@abc.com");
-        expect(manager.number).toEqual("1234567890");
-
-    })
+describe("Manager", () => {
+    describe("Initialization", () => {
+        it("should be create an object with parameter if provided valid arguments", () => {
+            const manager = new Manager ("Sarah", 1, "abc@abc.com", 1234567890);
+            const testValue = "Manager";
+            //expect(manager.name).toEqual("sarah");
+            //expect(manager.id).toEqual(1);
+            //expect(manager.email).toEqual("abc@abc.com");
+            expect(manager.number).toEqual(1234567890);
+            expect(manager.getRole()).toEqual(testValue);
+        });
+    });
+});
